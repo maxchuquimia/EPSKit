c730bd208efcdf6bfc844868a0c27eca16c3bdfa
Max Chuquimia <maxchuquimia@users.noreply.github.io>
2020-06-14 20:30:55 +1000

    Fix formula

--- Sources/EPSKit/Geometry/RegularPolygon.swift
+++ Sources/EPSKit/Geometry/RegularPolygon.swift
@@ -10,12 +10,13 @@ public extension EPS {
 
         public init(points count: Int, around center: CGPoint, radius: CGFloat) {
             guard count > 2 else { preconditionFailure("RegularPolygon must have 3 or more points") }
-            border = EPS.Line(
-                points: (0..<count).map({ idx in
-                    let angle = 360.0 / CGFloat(count) * CGFloat(idx)
-                    return CGPoint(x: (radius * sin(angle)) + center.x, y: (radius * cos(angle)) + center.y)
-                })
-            )
+            var _points: [CGPoint] = (0..<count).map({ idx in
+                let angle = 360.0 / CGFloat(count) * CGFloat(idx)
+                let r_angle = angle * .pi / 180
+                return CGPoint(x: center.x + (radius * cos(r_angle)), y: center.y + (radius * sin(r_angle)))
+            })
+            _points.append(_points.first!) // Close the loop
+            border = EPS.Line(points: _points)
         }
 
     }
